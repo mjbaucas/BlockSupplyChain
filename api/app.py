5661247f92005ed491c8c1c5bdc5a099a0605a55
mjbaucas <mjbaucas@gmail.com>
2021-06-07 18:06:49 -0400

    added api code

--- api/app.py
+++ api/app.py
@@ -4,9 +4,16 @@ import time
 
 app = Flask(__name__)
 
+temp = {'dev1': 10, 'dev2': 14, 'dev3': 15, 'dev4': 24}
+
 @app.route('/', methods=['GET'])
 def display_page():
-	return render_play('index.html')
+	return render_template('index.html', rfid_data=temp)
+
+@app.route('/send_rfid', methods=['POST'])
+def send_data():
+    response = request.get_json()
+    return "", 200
 
 if __name__=="__main__":
 	app.run(host='0.0.0.0', threaded=True, port=3000)
