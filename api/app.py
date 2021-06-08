eadee5f62d604f3e85097aecfa4d34cd84261565
mjbaucas <mjbaucas@gmail.com>
2021-06-08 15:26:35 -0400

    Added temperature and humidity client code. Also, moved some files around for better structure

--- api/app.py
+++ api/app.py
@@ -7,12 +7,14 @@ from dateutil import tz
 
 app = Flask(__name__)
 
-# Holds all RFID data
-temp = {}
-counter = 0
+# Holds all temp data
+temp_rfid = {}
+temp_th = {}
+rfid_counter = 0
+th_counter = 0
 
 # Temporary ledger for credentials
-temp_ledger = {"test_rfid_device_01": "password1234"}
+temp_ledger = {"test_rfid_device_01": "password1234", "test_temphumid_device_01": "password1234"}
 
 # Detect local time
 from_zone = tz.tzutc()
@@ -29,26 +31,38 @@ def convert_utc_to_local(utc):
 	return utc.replace(tzinfo=from_zone).astimezone(to_zone).strftime("%Y-%m-%d %H:%M:%S")
 
 def process_date(items):
-	temp_date_list = convert_utc_to_local(items[1]).split(" ")
-	items[1] = temp_date_list[0]
+	temp_date_list = convert_utc_to_local(items[-1]).split(" ")
+	items[-1] = temp_date_list[0]
 	items.append(temp_date_list[1])
 	return items
 
 @app.route('/', methods=['GET'])
 def display_page():
-	return render_template('index.html', rfid_data=temp)
+	return render_template('index.html', rfid_data=temp_rfid, th_data=temp_th)
 
-@app.route('/send_rfid', methods=['POST'])
-def send_data():
-	global counter
+@app.route('/send/rfid', methods=['POST'])
+def send_rfid_data():
+	global rfid_counter
 	response = json.loads(request.get_json())
 	if all (k in ["credentials", "data"] for k in response) and len(response) == 2:
 		credentials = response["credentials"]
 		if check_user(credentials["userid"], credentials["password"]):
-			counter+=1
-			temp.update({counter: process_date(response["data"])})
+			rfid_counter+=1
+			temp_rfid.update({rfid_counter: process_date(response["data"])})
 			return "", 200
 	return "", 500
+
+@app.route('/send/temphumid', methods=['POST'])
+def send_th_data():
+	global th_counter
+	response = json.loads(request.get_json())
+	if all (k in ["credentials", "data"] for k in response) and len(response) == 2:
+		credentials = response["credentials"]
+		if check_user(credentials["userid"], credentials["password"]):
+			rfid_counter+=1
+			temp_th.update({rfid_counter: process_date(response["data"])})
+			return "", 200
+	return "", 500 
 
 if __name__=="__main__":
 	app.run(host='0.0.0.0', threaded=True, port=3000)
---
+++ api/clients/sendTempHumid.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import time
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT11
+gpio = 24
+
+device_id = "test_temphumid_device_01"
+password = "password1234"
+
+url = 'http://192.168.2.221:3000/send_rfid'
+data = {}
+
+try:
+    while True: 
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+            if humidity is not None and temperature is not None:                        print(id)
+                print(text)
+                timestamp = int(round(time.time() * 1000))
+                packet = {"credentials":{"userid": device_id, "password": password}, "data": [humidity, temperature, timestamp]}
+                temp_value = request.post(url, json=json.dyumps(packet), headers={'Content-Type': 'application/json', 'X-Api-Key': ''})
+            except Exception:
+            pass
+except KeyboardInterrupt:
+    pass
+
+    
