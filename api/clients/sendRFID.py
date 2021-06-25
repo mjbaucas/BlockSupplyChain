a49056f4ee5e8701dd2f5464ee42186c0f5484b8
mjbaucas <mjbaucas@gmail.com>
2021-06-25 01:00:59 -0400

    accurate time

--- api/clients/sendRFID.py
+++ api/clients/sendRFID.py
@@ -21,9 +21,8 @@ try:
             #print(text)
             timestamp = datetime.now().timestamp()
             packet = {"credentials":{"userid": device_id, "password": password}, "data": {"tag": tag, "timestamp": timestamp}}
-            start = time.time()
             temp_value = requests.post(url, json=json.dumps(packet), headers={'Content-Type': 'application/json', 'X-Api-Key' : ''})
-            print('time taken ' + str((time.time()-start)*1000) + ' ms')
+            print('time taken: ' + str(temp_value.elapsed.total_seconds))
         except Exception as e:
             #print(e) # Uncomment for debugging  
             pass
