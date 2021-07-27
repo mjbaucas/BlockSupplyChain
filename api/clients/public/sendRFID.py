94dc0b4c2fe21759edf066d487aa8962da083b97
mjbaucas <mjbaucas@gmail.com>
2021-07-27 00:13:02 -0400

    Syntax fix

--- api/clients/public/sendRFID.py
+++ api/clients/public/sendRFID.py
@@ -30,7 +30,8 @@ while blockchain_key is None and not enabled:
     packet = {"userid": device_id}
     temp_value = requests.post(register_participant_url, json=json.dumps(packet), headers={'Content-Type': 'application/json', 'X-Api-Key' : ''})
     if temp_value.status_code == 200:
-        blockchain_key = temp_value.json["hashed_key"]
+        print(temp_value.json)
+        #blockchain_key = temp_value.json["hashed_key"]
         enabled = True
 
 try:
